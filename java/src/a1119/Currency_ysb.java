d4a9c95f8c919ed9d985bf26eafb9f271d2f4716
burdock-man <burdockman@naver.com>
2024-11-20 17:16:05 +0900

    241120.01

--- java/src/a1119/Currency_ysb.java
+++ java/src/a1119/Currency_ysb.java
@@ -8,21 +8,10 @@ public class Currency_ysb {
         System.out.print("\n\n금액을 입력하세요: ");
         int cash = scan.nextInt();
         int num = 0;
-        
-        int currency[] = new int[8];
-        currency[0] = 50000;
-        currency[1] = 10000;
-        currency[2] = 5000;
-        currency[3] = 1000;
-        currency[4] = 500;
-        currency[5] = 100;
-        currency[6] = 50;
-        currency[7] = 10;
-
+        int currency[] = {50000, 10000, 5000, 1000, 500, 100, 50, 10};
         for (int i = 0; i < currency.length; i++) {
             num = cash / currency[i];
             cash = cash - (num * currency[i]);
-
             if (currency[i] == currency[0]) System.out.printf("\n오만원권 : %d 매", num);
             if (currency[i] == currency[1]) System.out.printf("\n만원권   : %d 매", num);
             if (currency[i] == currency[2]) System.out.printf("\n오천원권 : %d 매", num);
@@ -32,6 +21,5 @@ public class Currency_ysb {
             if (currency[i] == currency[6]) System.out.printf("\n오십원   : %d 개", num);
             if (currency[i] == currency[7]) System.out.printf("\n십원     : %d 개", num);
         }
-        
     }
 }
--- java/src/a1120/HomeWork.java
+++ java/src/a1120/HomeWork.java
@@ -1,4 +1,4 @@
-package a1119;
+package a1120;
 
 import java.util.Scanner;
 
@@ -44,38 +44,45 @@ public class HomeWork {
         System.out.println("\n(201p 9번문제)=========================================");
 
         Scanner scan = new Scanner(System.in);
-        int grades3[] = new int[3];
+        int student3 = 0;
+        int[] grades3 = new int[student3];
         int sum3 = 0;
         int max3 = 0;
-        double avg3 = 0;
+        double avg3 = 0.0;
 
         while (true) {
-            System.out.println("\n.......................................................");
+            System.out.println("\n......................................................");
             System.out.println("1.학생수 : 2.점수입력 : 3.점수리스트 : 4.분석 : 5.종료");
-            System.out.println(".......................................................");
+            System.out.println("......................................................");
 
             System.out.print("\n선택> ");
-            int choice3 = scan.nextInt();
+            int choice3 = scan.nextInt(); // int choice3 = Integer.parseInt(scan.nextLin()); 이게 더 안정적
 
-            if(choice3 == 1) System.out.println("학생수> " + grades3.length);
+            if(choice3 == 1) {
+                student3 = 0; // 초기화
+                grades3 = new int[student3]; // 초기화
+                System.out.print("학생수> ");
+                student3 = scan.nextInt();
+                grades3 = new int[student3];
+            }
             if(choice3 == 2) {
-                for (int i = 0; i < grades3.length; i++) { // 초기화
+                for (int i = 0; i < student3; i++) { // 초기화
                         grades3[i] = 0;
                         max3 = 0;
                         sum3 = 0;
-                        avg3 = 0;
+                        avg3 = 0.0;
                 }
                 System.out.println("점수를 입력해주세요.");
-                for (int i = 0; i < grades3.length; i++) {
+                for (int i = 0; i < student3; i++) {
                     System.out.printf("학생%d의 성적> ",i + 1);
                     grades3[i] = scan.nextInt();
                     sum3 += grades3[i];
                     if (max3 < grades3[i]) max3 = grades3[i];
-                    avg3 =  (double) sum3 / grades3.length;
+                    avg3 =  (double) sum3 / student3;
                 }
             }
             if(choice3 == 3) {
-                for (int i = 0; i < grades3.length; i++) {
+                for (int i = 0; i < student3; i++) {
                     System.out.printf("학생%d의 성적> %d\n", i + 1, grades3[i]);
                 }
             }
