67885541e1346af4d97f2009eb1698514c20a143
burdock-man <burdockman@naver.com>
2024-11-21 17:17:15 +0900

    241121.02

---
+++ java/src/a1121/homework/Rectangle.java
@@ -0,0 +1,23 @@
+package a1121.homework;
+
+public class Rectangle {
+
+    // 필드 선언
+    int width = 0; // 초기값 0
+    int height = 0; // 초기값 0
+
+
+    // 생성자 선언
+
+    Ractangle() {};
+
+    Ractangle(int width) {
+        this.width = width;
+        this.height = height;
+    };
+
+    Ractangle(int width, int height) {
+        this.width = width;
+        this.height = height;
+    };
+}
