ba8f42f2fb7c6a50f650d2a9f8ca9c10d5dccf5d
burdock-man <burdockman@naver.com>
2024-11-24 19:40:12 +0900

    241124.01

--- java/src/a1122/homework/Student.java
+++ java/src/a1122/homework/Student.java
@@ -51,6 +51,8 @@ class Student {
         System.out.println("|                                                                                                  |");
         System.out.println("| 학생 리스트                                                                                      |");
 
+        sTotalClear();
+
         for (int j = 0; j < i.length; j++) {
             System.out.printf("| %d | 이름: %s | 학번: %d | 국어점수: %3d | 영어점수: %3d | 수학점수: %3d | 평균: %3.02f  |\n",
             i[j].number, i[j].name, i[j].id, i[j].koreanScore, i[j].englishScore, i[j].mathScore, i[j].average);
@@ -69,14 +71,6 @@ class Student {
         System.out.println("|                                                                                                  |");
     }
 
-    // 정보수정메서드
-    // public static void sMod(Student i) {
-
-
-    // }
-
-
-
     // 게터, 세터
     public String getName() {return name;}
     public void setName(String name) {this.name = name;}
@@ -85,27 +79,54 @@ class Student {
     public void setId(int id) {this.id = id;}
 
     public int getKoreanScore() {return koreanScore;}
-    public void setKoreanScore(int koreanScore) {this.koreanScore = koreanScore;}
+    public void setKoreanScore(int koreanScore) {this.koreanScore = koreanScore; updateAverage();}
 
     public int getEnglishScore() {return englishScore;}
-    public void setEnglishScore(int englishScore) {this.englishScore = englishScore;}
+    public void setEnglishScore(int englishScore) {this.englishScore = englishScore; updateAverage();}
 
     public int getMathScore() {return mathScore;}
-    public void setMathScore(int mathScore) {this.mathScore = mathScore;}
+    public void setMathScore(int mathScore) {this.mathScore = mathScore; updateAverage();}
 
     public int getNumber() {return number;}
     public double getAverage() {return average;}
 
+    public static int getTotalKorean() {return totalKorean;}
+    public static void setTotalKorean(int totalKorean) {Student.totalKorean = totalKorean;}
 
+    public static int getTotalEnglish() {return totalEnglish;}
+    public static void setTotalEnglish(int totalEnglish) {Student.totalEnglish = totalEnglish;}
 
+    public static int getTotalMath() {return totalMath;}
+    public static void setTotalMath(int totalMath) {Student.totalMath = totalMath;}
 
-
-
-
-
-
-
-
+    public static int getTotalStudent() {return totalStudent;}
     
+    public static void setTotalStudent(int totalStudent) {Student.totalStudent = totalStudent;}
+
+
+    private void updateAverage() {
+        this.average = (double) (this.koreanScore + this.englishScore + this.mathScore) / 3;
+    }
+
+    public static void updateTotalAverage(Student[] i) {
+
+        sTotalClear();
+    
+        for (Student j : i) {
+            totalKorean += j.getKoreanScore();
+            totalEnglish += j.getEnglishScore();
+            totalMath += j.getMathScore();
+        }
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public static void resetNumbers(Student[] i) {
+        for (int j = 0; j < i.length; j++) {
+            i[j].setNumber(j + 1); // 학생 번호를 배열 순서대로 재할당
+        }
+    }
 
 }
--- java/src/a1122/homework/StudentEx.java
+++ java/src/a1122/homework/StudentEx.java
@@ -36,7 +36,8 @@ public class StudentEx {
                 System.out.printf("| 리스트 번호> ");
                 int modSelect = scan.nextInt();
                 scan.nextLine(); // 개행문자 제거? 이걸 해야 다음 문자를 받을수 있나보다.
-                if (modSelect < 0 || modSelect > sArr.length) {
+
+                if (modSelect < 1 || modSelect > sArr.length) {
                     System.out.println("|                                                                                                  |");
                     System.out.printf("| 잘못된 리스트 번호입니다. 1부터 %d까지의 리스트 번호를 입력해 주세요.                             |\n", sArr.length);
                     System.out.println("|                                                                                                  |");
@@ -50,29 +51,118 @@ public class StudentEx {
                 String modName = scan.nextLine();
                 sArr[modSelect-1].setName(modName);
 
+                System.out.println("|                                                                                                  |");
+                System.out.printf("| 수정할 학생의 국어 성적을 입력해 주세요. (현재 국어 성적: %3s)                                   |\n", sArr[modSelect-1].getKoreanScore());
+                System.out.print("| 국어성적> ");
+                int modKorean = scan.nextInt();
+                if (modKorean >= 0 && modKorean <= 100) {
+                    sArr[modSelect-1].setKoreanScore(modKorean);
+                } else {
+                    System.out.println("|                                                                                                  |");
+                    System.out.println("| 잘못된 성적입니다. 1부터 100까지의 성적을 입력해 주세요. 현재 국어점수는 0점으로 처리됩니다.     |");
+                    System.out.println("|                                                                                                  |");
+                    sArr[modSelect-1].setKoreanScore(0);
+                }
 
-                // 이 아래부터 계속 하면 된다.
-                // 일단 이름 수정하는것까지는 구현했는데,
-                // 성적을 바꿔도 평균이 바뀌지 않는 문제가 생겼다.
+                System.out.println("|                                                                                                  |");
+                System.out.printf("| 수정할 학생의 영어 성적을 입력해 주세요. (현재 영어 성적: %3s)                                   |\n", sArr[modSelect-1].getEnglishScore());
+                System.out.print("| 영어성적> ");
+                int modEnglish = scan.nextInt();
+                if (modEnglish >= 0 && modEnglish <= 100) {
+                    sArr[modSelect-1].setEnglishScore(modEnglish);
+                } else {
+                    System.out.println("|                                                                                                  |");
+                    System.out.println("| 잘못된 성적입니다. 1부터 100까지의 성적을 입력해 주세요. 현재 영어점수는 0점으로 처리됩니다.     |");
+                    System.out.println("|                                                                                                  |");
+                    sArr[modSelect-1].setEnglishScore(0);
+                }
 
-                // System.out.println("|                                                                                                  |");
-                // System.out.printf("| 수정할 학생의 국어 성적을 입력해 주세요. (현재 국어 성적: %3s)                                   |\n", sArr[modSelect-1].getKoreanScore());
-                // int modKorean = scan.nextInt();
-                // if (modKorean >= 0 && modKorean <= 100) {
-                //     sArr[modSelect-1].setKoreanScore(modKorean);
-                // } else {
-                //     System.out.println("|                                                                                                  |");
-                //     System.out.println("| 잘못된 성적입니다. 1부터 100까지의 성적을 입력해 주세요. 현재 국어점수는 0점으로 처리됩니다.            |");
-                //     System.out.println("|                                                                                                  |");
-                //     sArr[modSelect-1].setKoreanScore(0);
-                // }
+                System.out.println("|                                                                                                  |");
+                System.out.printf("| 수정할 학생의 수학 성적을 입력해 주세요. (현재 수학 성적: %3s)                                   |\n", sArr[modSelect-1].getMathScore());
+                System.out.print("| 수학성적> ");
+                int modMath = scan.nextInt();
+                if (modMath >= 0 && modMath <= 100) {
+                    sArr[modSelect-1].setMathScore(modMath);
+                } else {
+                    System.out.println("|                                                                                                  |");
+                    System.out.println("| 잘못된 성적입니다. 1부터 100까지의 성적을 입력해 주세요. 현재 수학점수는 0점으로 처리됩니다.     |");
+                    System.out.println("|                                                                                                  |");
+                    sArr[modSelect-1].setMathScore(0);
+                }
 
-
-                Student.sTotalClear(); // 성적 초기화 해줘야지 누적이 안된다.
 
             } else if (select == 3) {
                 
+                scan.nextLine(); // 개행문자 제거
+
+                System.out.println("|                                                                                                  |");
+                System.out.println("| 생성할 학생의 이름을 입력해 주세요.                                                              |");
+                System.out.print("| 이름> ");
+                String newName = scan.nextLine();
+
+                System.out.println("|                                                                                                  |");
+                System.out.println("| 생성할 학생의 국어 성적을 입력해 주세요.                                                         |");
+                System.out.print("| 국어성적> ");
+                int newKorean = scan.nextInt();
+
+                System.out.println("|                                                                                                  |");
+                System.out.println("| 생성할 학생의 영어 성적을 입력해 주세요.                                                         |");
+                System.out.print("| 영어성적> ");
+                int newEnglish = scan.nextInt();
+
+                System.out.println("|                                                                                                  |");
+                System.out.println("| 생성할 학생의 수학 성적을 입력해 주세요.                                                         |");
+                System.out.print("| 수학성적> ");
+                int newMath = scan.nextInt();
+
+                Student[] newArr = new Student[sArr.length + 1]; // 크기 증가
+                for (int i = 0; i < sArr.length; i++) {
+                    newArr[i] = sArr[i];
+                }
+
+                newArr[sArr.length] = new Student(newName, newKorean, newEnglish, newMath);
+                sArr = newArr;
+
+                System.out.println("|                                                                                                  |");
+                System.out.println("| 새로운 학생이 추가되었습니다.                                                                  |");
+
             } else if (select == 4) {
+
+                Student.sInfo(sArr);
+                System.out.println("|                                                                                                  |");
+                System.out.println("| 삭제할 학생 선택                                                                                 |");
+                System.out.printf("| 리스트 번호> ");
+                int delSelect = scan.nextInt();
+
+                if (delSelect < 1 || delSelect > sArr.length) {
+                    System.out.println("|                                                                                                  |");
+                    System.out.printf("| 잘못된 리스트 번호입니다. 1부터 %d까지의 리스트 번호를 입력해 주세요.                             |\n", sArr.length);
+                    System.out.println("|                                                                                                  |");
+                    continue;
+                }
+
+                Student.setTotalKorean(Student.getTotalKorean() - sArr[delSelect - 1].getKoreanScore());
+                Student.setTotalEnglish(Student.getTotalEnglish() - sArr[delSelect - 1].getEnglishScore());
+                Student.setTotalMath(Student.getTotalMath() - sArr[delSelect - 1].getMathScore());
+
+
+                Student.setTotalStudent(Student.getTotalStudent() - 1);
+
+                System.out.println("|                                                                                                  |");
+                System.out.println("| 선택한 학생이 삭제되었습니다.                                                                  |");
+
+                Student[] newArr = new Student[sArr.length - 1];
+                int newIdx = 0;
+                for (int i = 0; i < sArr.length; i++) {
+                    if (i != delSelect - 1) {
+                        newArr[newIdx++] = sArr[i];
+                    }
+                }
+
+                sArr = newArr;
+
+                Student.resetNumbers(sArr);
+
                 
             } else if (select == 5) {
                 System.out.println("| 프로그램 종료                                                                                    |");
